4413256395881e118a971515663945f0bd1194ce
Armin0323 <80569738+Armin0323@users.noreply.github.com>
2021-06-09 03:56:47 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,85 +3,119 @@
 def adder(currentValue):         #Declares a function which will be used to add numbers together and takes in an argument
 
     #Prompts the user to enter a number and accepts input from the user, typecasts it to a float
-    print("Please enter another number for the adder")
-    add = float(input())
+    print("What number would you like to add to {:.4f}?".format(currentValue))
+    try:                             #The try statement here is to try to see whether or not there are errors
+        add = float(input())
 
-    #adds the inputted value to the value already stored in the calculator and returns the sum
-    sum = currentValue + add
-    return sum
+        #adds the inputted value to the value already stored in the calculator and returns the sum
+        sum = currentValue + add
+        return sum
+    except:                          #catches if the user fails to input a number
+        print("You did not enter a number")        #informs the user they didn't enter a number
+        sum = adder(currentValue)     #recursively calls the function to prompt the user again
+        return sum                    #returns the correct value as the sum
 
-def subtractor(currentVaule):
+def subtractor(currentValue):
    
     #prompts the user to enter a number and accepts input from the user, typecasts it to a float
-    print("Please enter another number for the subtractor")
-    subtract = float(input())
+    print("What number would you like to subtract from {:.4f}?".format(currentValue))
+    try:
+        subtract = float(input())
 
-    #Takes the difference between the value stored in the calculator and subtract and returns the difference
-    difference = currentVaule - subtract
-    return difference
+        #Takes the difference between the value stored in the calculator and subtract and returns the difference
+        difference = currentValue - subtract
+        return difference
+    except:
+        print("You did not enter a number.")
+        difference = subtractor(currentValue)
+        return difference
 
 def multiplier(currentValue):      #Declares the multiplier function which multiplies two numbers together, takes current value in calculator as an argument
 
     #prompts the user to enter a number and accepts input from the user, typecasts it to a float
-    print("Please enter a number for the multiplier")
-    multiple = float(input())
+    print("What number would you like to multiply to {:.4f}?".format(currentValue))
+    try:
+        multiple = float(input())
 
-    #Takes the product of the stored value in the calculator and the user inputted number and returns the product
-    product = currentValue * multiple
-    return product
+        #Takes the product of the stored value in the calculator and the user inputted number and returns the product
+        product = currentValue * multiple
+        return product
+    except:
+        print("You did not enter a number.")
+        product = multiplier(currentValue)
+        return product
 
 def divider(currentValue):         #Declares a function divider which divides two numbers and returns the quotient
 
     #prompts the user to input another number and accepts it, typecasts it to a float
-    print("Please enter another number for the divider")
-    divide = float(input())
+    print("What number would you like to divide {:.4f} by?".format(currentValue))
+    try:
+        divide = float(input())
 
-    #Takes the quotient of the stored value in the calculator and the user inputted number and returns the quotient
-    quotient = currentValue/divide
-    return quotient
+        #Takes the quotient of the stored value in the calculator and the user inputted number and returns the quotient
+        quotient = currentValue/divide
+        return quotient
+    except:
+        print("You did not enter a number or you tried to divide by zero.")
+        quotient = divider(currentValue)
+        return quotient
 
 def exponent(currentValue):          #declares an exponent function that takes in the current value stored in the calculator as an argument
 
     #Print statement to prompt the user for an input, and takes in user input into variable power and casts it as an int
-    print("Please enter an integer of what power you would like to raise ", currentValue, " to")
-    power = int(input())
+    print("What integer would you like to raise {:.4f} to?".format(currentValue))
+    try:
+        power = int(input())
 
-    #establishes a counter valuable for the for loop
-    counter = 1
+        #establishes a counter valuable for the for loop
+        counter = 1
 
-    #establishes a temporary value to store the calculations in
-    tempValue = currentValue
+        #establishes a temporary value to store the calculations in
+        tempValue = currentValue
 
-    #condition that checks if power is 0 in order to return 1 no matter what
-    if power == 0:
-        return 1
+        #condition that checks if power is 0 in order to return 1 no matter what
+        if power == 0:
+            return 1
 
-    #condition to check if power is 1, it will return the same value
-    elif power == 1:
-        return currentValue
+        #condition to check if power is 1, it will return the same value
+        elif power == 1:
+            return currentValue
 
-    #condition to check if power is less than 0, then it loops and divides the value
-    elif power < 0:
-        power = abs(power)
-        for x in range(counter,power):
-            tempValue = tempValue/currentValue
-        currentValue = tempValue
-        return currentValue
+        #condition to check if power is less than 0, then it loops and divides the value
+        elif power < 0:
+            power = abs(power)
+            for x in range(counter - 2,power):
+                tempValue = tempValue/currentValue
+            currentValue = tempValue
+            return currentValue
 
-    #condition to check if power is greater than one, if so it multiplies current value in the forloop.
-    elif power > 0:
-        for x in range(counter, power):
-            tempValue = tempValue * currentValue
-        currentValue = tempValue
-        return currentValue
-    else:
-        return currentValue
+        #condition to check if power is greater than one, if so it multiplies current value in the forloop.
+        elif power > 0:
+            for x in range(counter, power):
+                tempValue = tempValue * currentValue
+            currentValue = tempValue
+            return currentValue
+        else:
+            return currentValue
+    except:
+        print("You did not enter an integer.")
+        raisedValue = exponent(currentValue)
+        return raisedValue
+
+def initialize():        #The initializer function 
+    print("Please enter a number for the calculator: ")
+    try:
+        initValue = float(input())
+        return initValue
+    except:
+        print("Error, did not enter a number.")
+        initValue = initialize()
+        return initValue
 
 def main():            #Declares the main function
 
-    #Prompts the user to enter a number and accepts input from the user
-    print("Please enter a number for the calculator")
-    currentValue = float(input())
+    #Calls the initialize function to initialize the calculator with an initial value
+    currentValue = initialize()
 
     while(1):            #While loop to keep the calculator running
 
@@ -108,7 +142,7 @@ def main():            #Declares the main function
             currentValue = adder(currentValue)
 
             #Displays the sum
-            print("The sum is: ", currentValue)
+            print("The sum is: {:.4f}".format(currentValue))
 
         #This is the second option, in line with the subtractor    
         elif userOpt == '2':
@@ -117,7 +151,7 @@ def main():            #Declares the main function
             currentValue = subtractor(currentValue)
 
             #displays the difference
-            print("The difference is: ", currentValue)
+            print("The difference is: {:.4f}" .format(currentValue))
         
         #This catches option 3, the multiplier
         elif userOpt == '3':
@@ -126,7 +160,7 @@ def main():            #Declares the main function
             currentValue = multiplier(currentValue)
 
             #Displays the product
-            print("The Product is: ", currentValue)
+            print("The Product is: {:.4f}".format(currentValue))
 
         #catches option 4, the divider
         elif userOpt == '4':
@@ -135,7 +169,7 @@ def main():            #Declares the main function
             currentValue = divider(currentValue)
 
             #displays the quotient
-            print("The quotient is: ", currentValue)
+            print("The quotient is: {:.4f}".format(currentValue))
 
         #catches option 5, clearing the calculator
         elif userOpt == '5':
@@ -149,7 +183,7 @@ def main():            #Declares the main function
 
             #takes the value stored in the calculator and passes it to exponent function
             currentValue = exponent(currentValue)
-            print("The answer is: ", currentValue)
+            print("The answer is: {:.4f}".format(currentValue))
 
         #catches Option 7, quitting, and breaks from the while loop
         elif userOpt == '7':
